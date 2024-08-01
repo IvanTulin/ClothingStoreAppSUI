693486e70ca876ce59641d13151e0dfab36af6e1
IvanTulin <tulin.v@bk.ru>
2024-08-01 14:33:19 +0300

    Created a developer branch

---
+++ ClothingStoreAppSUI/ClothingStoreAppSUI/ClothingStoreAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingStoreAppSUIApp.swift
+//  ClothingStoreAppSUI
+//
+//  Created by Ivan Tulin on 01.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ClothingStoreAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
